2740c9f8d1d32de7568a31ab22756f2a8402107d
skypyb <pyb068@gmail.com>
2018-10-19 19:24:36 +0800

    一点小修改

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ test 包下有大概使用方式
   
 #### v1.0.8(2018-10-19)  
 时隔四个月的更新。去除了无用文件。  
+编辑器对名字为 isXxxx 的属性进行 getter、setter 生成时会抹去 is，针对这种会出现异常的情况作了处理( BeanUtil.java 文件下)  
+在 PrimitiveHandler.java 文件下添加了对于 boolean 的包装类 Boolean 的反射处理
 #### v1.0.7(2018-06-04)  
 目前上传版本，之前的修改都不记得了，暂时没发现有什么bug  
 
@@ -20,7 +22,9 @@ test 包下有大概使用方式
 
 * QueryTableDAO类：万能DAO方法（单表），实现了对单表的CRUD以及条件查询等，通过调用不同的方法返回不同的结果，需要表字段名和类属性名一致，并且顺序一致
 
-* TypeHandlerFactory类：数据处理，实现了一个Factory模式，传入一个需要转换的Object对象和转换后的数据类型Class，实现类型的转换  
+* TypeHandlerFactory类：数据处理，实现了一个Factory模式，传入一个需要转换的Object对象和转换后的数据类型Class，实现类型的转换 
+<br> 
+<br> 
 `目前实现的类型转换`:
      >基本数据类型及其包装类以及String类互相转换  
      >结果集转换为一个对象  
--- src/top/yibobo/util/BeanUtil.java
+++ src/top/yibobo/util/BeanUtil.java
@@ -9,54 +9,60 @@ import top.yibobo.util.handler.PrimitiveHandler;
 import top.yibobo.util.handler.TypeHandlerFactory;
 
 public class BeanUtil {
-	
-	/**
-	 * 此方法获取传入的对象实例和传入的map
-	 * 通过反射将实例的属性名取出以此作为Key获取map中的value值
-	 * 并将其动态的进行类型转换后给对象赋值
-	 * 需要条件:对象属性值在Map的Key中有对应的值
-	 * @author pyb
-	 * @param obj
-	 * @param Map<String,？>
-	 */
-	public void populate(Object obj,Map<String,?> map) {
-		Class type = obj.getClass();//获取传入对象其类型
-		Field[] fields = type.getDeclaredFields();//得到其所有的属性值
-		for(Field f:fields) {
-			
-			//将属性名字作为键值取出map中的对象
-			Object value = map.get(f.getName());
-			//如果value为空则跳过
-			if(value ==null) {
-				continue;
-			}
-			//如果value是个数组的话，就把他第一个单位取出来作为value
-			if(value.getClass().isArray()) {
-				Object[] array = (Object[]) map.get(f.getName());
-				value = array[0];
-			}
-			//获取该属性的set方法名字
-			String fieldName = "set"+f.getName().substring(0, 1).toUpperCase()+f.getName().substring(1);
-			
-			try {
-				//实例化该set方法
-				Method m = obj.getClass().getMethod(fieldName, f.getType());
-				
-				//管他是啥类型，作为调用者只需要用就行了，具体new出哪个处理器让工厂来判断
-				Object val = TypeHandlerFactory.creatTypeHandler(f.getType()).typeHandler(f.getType(), value);
-				m.invoke(obj, val);//调用set方法给实例赋值
-				
-			} catch (NoSuchMethodException e) {
-				e.printStackTrace();
-			} catch (SecurityException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			} catch (IllegalArgumentException e) {
-				e.printStackTrace();
-			} catch (InvocationTargetException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+
+    /**
+     * 此方法获取传入的对象实例和传入的map
+     * 通过反射将实例的属性名取出以此作为Key获取map中的value值
+     * 并将其动态的进行类型转换后给对象赋值
+     * 需要条件:对象属性值在Map的Key中有对应的值
+     *
+     * @param obj
+     * @param map<String,？>
+     * @author pyb
+     */
+    public void populate(Object obj, Map<String, ?> map) {
+        Class type = obj.getClass();//获取传入对象其类型
+        Field[] fields = type.getDeclaredFields();//得到其所有的属性值
+        for (Field f : fields) {
+
+            //将属性名字作为键值取出map中的对象
+            Object value = map.get(f.getName());
+            //如果value为空则跳过
+            if (value == null) {
+                continue;
+            }
+            //如果value是个数组的话，就把他第一个单位取出来作为value
+            if (value.getClass().isArray()) {
+                Object[] array = (Object[]) map.get(f.getName());
+                value = array[0];
+            }
+            //获取该属性的set方法名字
+            String fieldName = "set" + f.getName().substring(0, 1).toUpperCase() + f.getName().substring(1);
+            if (fieldName.startsWith("setIs")) {//对编辑器自动生成 boolean 参数 Setter 的特殊处理
+                fieldName.replaceFirst("Is", "");
+            }
+
+            Method m = null;
+            try {
+                //实例化该set方法
+                m = obj.getClass().getMethod(fieldName, f.getType());
+
+                //管他是啥类型，作为调用者只需要用就行了，具体new出哪个处理器让工厂来判断
+                Object val = TypeHandlerFactory.creatTypeHandler(f.getType()).typeHandler(f.getType(), value);
+                m.invoke(obj, val);//调用set方法给实例赋值
+
+            } catch (NoSuchMethodException e) {
+                if (f.getType().getName().equals("boolean"))
+                e.printStackTrace();
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
