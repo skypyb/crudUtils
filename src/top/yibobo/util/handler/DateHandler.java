eecf919a09e13b5ab94fe0c4d82b993ab24494e3
skypyb <pyb068@gmail.com>
2018-11-23 22:17:03 +0800

    【CrudUtils】handler包下的数据处理类中的日期时间处理类修改。从以往的只有单项String转Date基础上，增加了Date及其子类向String的转换。创建数据处理器的factory中增加了Date子类的判定。

--- src/top/yibobo/util/handler/DateHandler.java
+++ src/top/yibobo/util/handler/DateHandler.java
@@ -1,44 +1,51 @@
-package top.yibobo.util.handler;
+package com.rocket.util.beanUtils.handler;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DateHandler implements TypeHandler {
-	protected DateHandler() {}
-	
-	
-	/**
-	 * 此方法实现将传入的value转换成指定的Date值
-	 */
-	@Override
-	public Object typeHandler(Class type, Object value) {
-		//判断是否赋值兼容，直接兼容的话就没必要往下了
-		if(type.isInstance(value)) {
-			return value;
-		}
-		
-		//如果value是个String类型的，就进行到字符串转日期的操作
-		if(String.class.isInstance(value)) {
-			String date = value.toString();
-			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd"); 
-			//得到java.util.Date类型和java.sql.Date类型的时间
-			java.util.Date date1 = null; 
-			try { 
-				date1 = format.parse(date); 
-			} catch (Exception e) { 
-				e.printStackTrace(); 
-			} 
-			java.sql.Date date2 = new java.sql.Date(date1.getTime()); 
-			
-			if(type.getName().equals("java.sql.Date")) {
-				return date2;
-			}
-			if(type.getName().equals("java.util.Date")) {
-				return date1;
-			}
-			
-		}
-		return null;
-		
-	}
+    protected DateHandler() {
+    }
+
+
+    /**
+     * 此方法实现将传入的value转换成指定的Date值
+     */
+    @Override
+    public Object typeHandler(Class type, Object value) {
+        //判断是否赋值兼容，直接兼容的话就没必要往下了
+        if (type.isInstance(value)) {
+            return value;
+        }
+
+        if (type == String.class && Date.class.isAssignableFrom(value.getClass())) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            return sdf.format(value);
+        }
+
+        //如果value是个String类型的，就进行到字符串转日期的操作
+        if (String.class.isInstance(value)) {
+            String date = value.toString();
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            //得到java.util.Date类型和java.sql.Date类型的时间
+            java.util.Date date1 = null;
+            try {
+                date1 = format.parse(date);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            java.sql.Date date2 = new java.sql.Date(date1.getTime());
+
+            if (type.getName().equals("java.sql.Date")) {
+                return date2;
+            }
+            if (type.getName().equals("java.util.Date")) {
+                return date1;
+            }
+
+        }
+        return null;
+
+    }
 
 }
